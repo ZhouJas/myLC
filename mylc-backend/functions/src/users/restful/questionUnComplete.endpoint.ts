daeffda27e7fd09b723b2048172fd5d570540491
Jason Zhou <65838712+ZhouJas@users.noreply.github.com>
2022-09-15 22:44:32 -0400

    feat(app): add frontend, rename backend folder

---
+++ mylc-backend/functions/src/users/restful/questionUnComplete.endpoint.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { Post } from 'firebase-backend'; // Get, Post, Put, Update, Delete available
+
+// Use the `Post` class which is extended from the `Endpoint` class.
+export default new Post((request: Request, response: Response) => {
+  // Read the values out of the body
+  const questionId = request.body['questionId'];
+
+  // Send your response. 201 to indicate the creation of a new resource
+  console.log("question marked as unfinished");
+  return response.status(200).send({
+    token: questionId,
+  });
+});
