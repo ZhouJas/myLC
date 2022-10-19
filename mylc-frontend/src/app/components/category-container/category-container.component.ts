6c1255064efbfe879c5178bdd7d6ce25878c9ba9
Jason Zhou <65838712+ZhouJas@users.noreply.github.com>
2022-10-19 17:24:40 -0400

    fix(animation): improve animation consistency

--- mylc-frontend/src/app/components/category-container/category-container.component.ts
+++ mylc-frontend/src/app/components/category-container/category-container.component.ts
@@ -1,12 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Problem, RecType } from 'src/app/Problem';
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger
-} from '@angular/animations';
 import { Subscription, Observable } from 'rxjs';
 
 const DEFAULT_DURATION = 400;
@@ -14,15 +7,7 @@ const DEFAULT_DURATION = 400;
 @Component({
   selector: 'app-category-container',
   templateUrl: './category-container.component.html',
-  styleUrls: ['./category-container.component.scss'],
-  animations: [
-    trigger('expanded', [
-      state('true', style({})),
-      state('false', style({ height: '0px' })),
-      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-in-out')),
-      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in-out'))
-    ])
-  ]
+  styleUrls: ['./category-container.component.scss']
 })
 export class CategoryContainerComponent implements OnInit {
   @Input() problems: Problem[] = []
